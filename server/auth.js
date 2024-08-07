6d5491891ac799d8d5ee893f4dace98cdfbb1415
Corentin Pradier <corentinpradier@users.noreply.github.com>
2024-08-07 12:06:48 +0200

    Ajout d'un onglet log in

--- server/auth.js
+++ server/auth.js
@@ -1,38 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const loginForm = document.getElementById('loginForm');
-    if (loginForm) {
-        loginForm.addEventListener('submit', function(event) {
-            event.preventDefault();
-
-            const username = document.getElementById('username').value;
-            const password = document.getElementById('password').value;
-
-            // Soumettre les informations de connexion au serveur
-            fetch('/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: new URLSearchParams({
-                    username: username,
-                    password: password
-                })
-            }).then(response => {
-                if (response.redirected) {
-                    window.location.href = response.url;
-                } else {
-                    alert('Invalid credentials.');
-                }
-            });
-        });
+function redirectToPage() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+    // Remplacez 'votreUsername' et 'votrePassword' par le nom d'utilisateur et le mot de passe attendus
+    if (username === 'vigo' && password === 'vigo') {
+        window.location.href = '../private/index.html'; // Remplacez par l'URL de votre nouvelle page
+        return false;
+    } else {
+        alert("Nom d'utilisateur ou mot de passe incorrect. Veuillez réessayer.");
+        return false; // Empêche le formulaire de soumettre
     }
-
-    const logoutButton = document.getElementById('logoutButton');
-    if (logoutButton) {
-        logoutButton.addEventListener('click', function() {
-            fetch('/logout', { method: 'POST' }).then(() => {
-                window.location.href = '/login.html';
-            });
-        });
-    }
-});
+}
